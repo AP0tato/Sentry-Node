8928e7b871469d995d339b17a79d849d02cac3b9
AP0tato <aliaid0207@gmail.com>
2026-05-06 15:07:40 -0400

    Sentry Node

---
+++ main.py
@@ -0,0 +1,39 @@
+from ultralytics import YOLO
+import time
+from collections import Counter
+import cv2
+
+def move_servo(angle):
+    print("[SERVO] Rotate", angle, "degrees")
+    
+def pump(delay):
+    print("[PUMP] Moves stepper forward for", delay, "seconds")
+    time.sleep(delay)
+    
+def take_photo():
+    print("[Camera] Taking photo")
+    time.sleep(1)
+    
+model = YOLO("yolov8s.pt")
+
+def train_model():
+    results = model.train(
+        data = "datasets/Microplastics.v3i.yolov8/data.yaml",
+        epochs = 100,
+        imgsz = 1280,
+        batch = 8,
+        name = "microplastics_v1"
+    )
+    
+    print(f"mAP50:     {results.results_dict['metrics/mAP50(B)']:.4f}")
+    print(f"mAP50-95:  {results.results_dict['metrics/mAP50-95(B)']:.4f}")
+    print(f"Precision: {results.results_dict['metrics/precision(B)']:.4f}")
+    print(f"Recall:    {results.results_dict['metrics/recall(B)']:.4f}")
+
+if __name__ == '__main__':
+    # while True:
+    #     move_servo(0)
+    #     pump(60)
+    #     take_photo()
+    #     pump(15)
+    train_model()
